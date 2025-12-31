12dcccf68215f75d9ca438124f3329ac2b744bd1
fratjo <francois.tjoens.pro@gmail.com>
2025-12-31 15:53:08 +0100

    chore: clean up

--- src/Blazor/Blazor/Blazor.Client/wwwroot/theme.js
+++
@@ -1,9 +0,0 @@
-﻿window.themeManager = {
-    setTheme: (themeName) => {
-        document.body.setAttribute('data-theme', themeName);
-        localStorage.setItem('theme', themeName);
-    },
-    getTheme: () => {
-        return localStorage.getItem('theme') || 'light';
-    }
-};
