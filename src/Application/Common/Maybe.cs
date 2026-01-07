cd475ea8aa7df92ddea0f5130b548d8f9a637d43
fratjo <francois.tjoens.pro@gmail.com>
2026-01-07 18:18:27 +0100

    Add ctor to some/none classes

--- src/Application/Common/Maybe.cs
+++ src/Application/Common/Maybe.cs
@@ -2,15 +2,22 @@
 
 public abstract class Maybe<T>
 {
-    public static Maybe<T> Some(T value) => new Some<T>(value);
-    public static Maybe<T> None() => new None<T>();
+    // If value is null, return None<T>
+    public static Maybe<T> Some(T value) => value is null ? None() : new Some<T>(value);
+    public static Maybe<T> None() => None<T>.Instance;
 }
 
-public sealed class Some<T>(T value) : Maybe<T>
+public sealed class Some<T> : Maybe<T>
 {
-    public T Value { get; } = value;
+    public T Value { get; }
+    internal Some(T value) 
+    {
+        Value = value;
+    }
 }
 
 public sealed class None<T> : Maybe<T>
 {
+    internal static readonly None<T> Instance = new None<T>();
+    private None() { }
 }
