79ec7eea5d22b7855a555cc46ff92d5b2c15244b
fratjo <francois.tjoens.pro@gmail.com>
2026-01-07 18:37:00 +0100

    Add Match function

--- src/Application/Common/Maybe.cs
+++ src/Application/Common/Maybe.cs
@@ -2,22 +2,23 @@
 
 public abstract class Maybe<T>
 {
-    // If value is null, return None<T>
     public static Maybe<T> Some(T value) => value is null ? None() : new Some<T>(value);
     public static Maybe<T> None() => None<T>.Instance;
+    public abstract TResult Match<TResult>(Func<T, TResult> some, Func<TResult> none);
 }
 
 public sealed class Some<T> : Maybe<T>
 {
     public T Value { get; }
-    internal Some(T value) 
-    {
-        Value = value;
-    }
+    internal Some(T value) => Value = value;
+    public override TResult Match<TResult>(Func<T, TResult> some, Func<TResult> none)
+        => some(Value);
 }
 
 public sealed class None<T> : Maybe<T>
 {
     internal static readonly None<T> Instance = new None<T>();
     private None() { }
+    public override TResult Match<TResult>(Func<T, TResult> some, Func<TResult> none)
+        => none();
 }
